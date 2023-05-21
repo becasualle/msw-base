0733c77fa03d3ff49d79c26bd3cd4503d58d8f81
Nelyubin Mark <mark.nelubin@flant.com>
2023-05-21 17:12:20 +0300

    Mocking REST API: Response Resolver

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,45 @@
 import { rest } from 'msw'
 
+/**
+ * List of request handlers.
+ * Each handler contains method, path, and a function that would return the mocked response.
+ */
 export const handlers = [
-  rest.post('/login', null),
-  rest.get('/user', null)
+  /**
+   * Mocks the response for the '/login' POST request.
+   *
+   * @param {object} req - Information about the matching request.
+   * @param {object} res - Functional utility to create the mocked response.
+   * @param {object} ctx - Group of functions to set the status code, headers, body, etc., of the mocked response.
+   * @returns {object} The mocked response with a status code.
+   */
+
+  rest.post('/login', (req, res, ctx)=>{
+    // Persist user's authentication in the session
+    sessionStorage.setItem('is-authenticated', 'true');
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200)
+    )
+  }),
+
+  rest.get('/user', (req, res, ctx)=>{
+    // Check if the user is authenticated in this session
+    const isAuthenticated = sessionStorage.getItem('is-authenticated');
+
+    if (!isAuthenticated){
+      // If not authenticated, respond with a 403 error
+      return res(
+          ctx.status(403),
+          ctx.json({errorMessage: 'Not authorized'})
+        )
+    }
+     // If authenticated, return a mocked user details
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: 'admin'
+      })
+    )
+  })
 ]
