d299455bd2a32744cbc0ad56d038722f4b40a6cb
Nelyubin Mark <mark.nelubin@flant.com>
2023-05-21 21:35:39 +0300

    Configure worker

---
+++ src/mocks/browser.js
@@ -0,0 +1,8 @@
+import { setupWorker } from 'msw'
+import { handlers } from './handlers'
+
+/**
+ * This configures a Service Worker with the given request handlers.
+ * Creates a worker instance with our request handlers
+ */
+export const worker = setupWorker(...handlers)
